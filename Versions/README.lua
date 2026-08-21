e9873fc7f8b6e33bbea485ef28d94d5acabc0a44
Thaw <thawuxen@gmail.com>
2026-08-21 16:49:27 +0100

    Revise README with project updates and licensing info
    Updated README with project status and notes.

---
+++ Versions/README.lua
@@ -0,0 +1,10 @@
+--[[
+
+  @Jean @Eris
+
+  Moved files to licensing site
+  Host is in the GC
+  Key is in CF
+  New loader is fully working now, just waiting on Achilles for the UI
+  (Which may take a few years, given that we hired Achilles for some fucking reason)
+  ]]
